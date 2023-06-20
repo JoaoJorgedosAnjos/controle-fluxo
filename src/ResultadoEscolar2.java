4ec1dfd845d52bf5b6d093e6e77859165b1ba849
JoaoJorge <joaojorgedosanjos@gmail.com>
2023-06-20 08:12:50 -0300

    Condicional encadeada e condiçã ternária

--- src/ResultadoEscolar2.java
+++ src/ResultadoEscolar2.java
@@ -2,11 +2,20 @@ public class ResultadoEscolar2 {
     public static void main(String[] args) {
         int nota = 6;
 
+        //Condicional encadeada
         if (nota >=7)
-            System.out.printf("Aprovadp");
+            System.out.println("Aprovadp");
         else if (nota >= 5 && nota < 7)
-            System.out.printf("Recuperação");
+            System.out.println("Recuperação");
         else
-            System.out.printf("Reprovado");
+            System.out.println("Reprovado");
+
+        //Condição ternária
+        //ex1
+        String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+        //ex2
+        String resultado1 = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado1);
     }
 }
